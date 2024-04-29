1d01c1910bda879ab4f1d01f9cae9ac1b548c9d5
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-04-29 17:04:10 +0200

    Create Basic Structure: Main View Collection View / Custom Main Collection View

---
+++ Dronete/Dronete/UI/MainView/MainCollectionViewCell/MainCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainCollectionViewCell.swift
+//  Dronete
+//
+//  Created by Daniel Cazorro Frías on 29/4/24.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "MainCollectionViewCell"
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var ivImage: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    
+    // MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- Dronete/Dronete/UI/MainView/MainViewController.swift
+++ Dronete/Dronete/UI/MainView/MainViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var cvMainCollectionView: UICollectionView!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        cvMainCollectionView.delegate = self
+        cvMainCollectionView.dataSource = self
     }
 
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
-    */
+}
 
+extension MainViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
 }
